3ce3c566f9b868fca4e74f5b57c50440b20250e5
yaya <merosyaputri@student.unp.ac.id>
2024-11-21 00:56:32 +0700

    Add files via upload

---
+++ JOOB SHEET 2/modul2.4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int p = 10;
+    int l = 5;
+    int luaspersegipanjang = p * l;
+
+    // menampilkan data yangg dimasukkan
+    printf("luas persegi panjang: %i", luaspersegipanjang);
+
+    return 0;
+}
