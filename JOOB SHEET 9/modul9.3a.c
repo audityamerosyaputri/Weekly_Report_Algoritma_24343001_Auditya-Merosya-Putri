633ce7aa0641d344ed0e80b11be1b4c602fa352e
yaya <merosyaputri@student.unp.ac.id>
2024-11-28 14:50:20 +0700

    Add files via upload

---
+++ JOOB SHEET 9/modul9.3a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int Lesley = 57082;
+    int Layla = Lesley;
+    int Balmond = Layla + 1;
+
+    printf("nilai Layla = %d\n", Layla);
+    printf("nilai Balmond = %d\n", Balmond);
+
+    return 0;
+}
